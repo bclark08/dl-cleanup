0b742fbaf65e15427079054506cd1d200973a5c3
Bradley Clark <45517982+bclark08@users.noreply.github.com>
2026-02-12 21:01:45 -0500

    Create cleanup.sh
    Initial commit.
    
    Creates five directories: VIDEOS, PHOTOS, DOCUMENTS, DATA, and DISK IMAGES.
    
    Then moves all files, based on file extension, into the appropriate directory.

---
+++ cleanup.sh
@@ -0,0 +1,19 @@
+#! /bin/zsh
+
+mkdir ~/Downloads/VIDEOS;
+mkdir ~/Downloads/PHOTOS;
+mkdir ~/Downloads/DOCUMENTS;
+mkdir ~/Downloads/DISK_IMAGES;
+mkdir ~/Downloads/DATA;
+mv ~/Downloads/*.mp4 ~/Downloads/VIDEOS;
+mv ~/Downloads/*.jpg ~/Downloads/PHOTOS;
+mv ~/Downloads/*.jpeg ~/Downloads/PHOTOS;
+mv ~/Downloads/*.tif ~/Downloads/PHOTOS;
+mv ~/Downloads/*.webp ~/Downloads/PHOTOS;
+mv ~/Downloads/*.tiff ~/Downloads/PHOTOS;
+mv ~/Downloads/*.png ~/Downloads/PHOTOS; 
+mv ~/Downloads/*.csv ~/Downloads/DATA;
+mv ~/Downloads/*.xlsx ~/Downloads/DATA;
+mv ~/Downloads/*.pdf ~/Downloads/DOCUMENTS;
+mv ~/Downloads/*.docx ~/Downloads/DOCUMENTS;
+mv ~/Downloads/*.dmg ~/Downloads/DISK_IMAGES
